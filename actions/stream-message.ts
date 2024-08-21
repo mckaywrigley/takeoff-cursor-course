8c007630ffce006c8263e5f56d4b230a23e8703c
Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>
2024-08-21 02:28:26 -0600

    update ai sdk

--- actions/stream-message.ts
+++ actions/stream-message.ts
@@ -1,25 +1,21 @@
 "use server";
 
 import { ChatMessage } from "@/app/chat/page";
+import { openai } from "@ai-sdk/openai";
+import { streamText } from "ai";
 import { createStreamableValue } from "ai/rsc";
-import OpenAI from "openai";
-
-const openai = new OpenAI();
 
 export async function streamMessage(messages: ChatMessage[]) {
   const stream = createStreamableValue("");
 
   (async () => {
-    const completion = await openai.chat.completions.create({
-      model: "gpt-4o-mini",
-      messages: [{ role: "system", content: "You are a helpful assistant." }, ...messages],
-      stream: true
+    const { textStream } = await streamText({
+      model: openai("gpt-4o-mini"),
+      messages: [{ role: "system", content: "You are a helpful assistant." }, ...messages]
     });
 
-    for await (const chunk of completion) {
-      if (chunk.choices[0].delta.content) {
-        stream.update(chunk.choices[0].delta.content);
-      }
+    for await (const delta of textStream) {
+      stream.update(delta);
     }
 
     stream.done();
